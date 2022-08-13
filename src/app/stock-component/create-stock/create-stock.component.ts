b169051d00eb13996f824f9da365edb18b596abd
endungu <endungu@jambopay.co.ke>
2022-08-13 21:13:25 +0300

    feat:reset form, patch form

--- src/app/stock-component/create-stock/create-stock.component.ts
+++ src/app/stock-component/create-stock/create-stock.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { StockModel } from '../../models/stock-model';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+let counter = 1;
 
 @Component({
   selector: 'app-create-stock',
@@ -8,22 +15,54 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-stock.component.scss'],
 })
 export class CreateStockComponent implements OnInit {
-  public stock: StockModel;
-  public confirmed: boolean = false;
-  public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
-  public nameControl = new FormControl();
-  public stockForm = new FormGroup({
-    name: new FormControl(null, Validators.required),
-    code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
-    price: new FormControl(0, [Validators.required, Validators.min(0)]),
-  });
-  constructor() {
-    this.stock = new StockModel('', '', 0, 0);
+  public stockForm!: FormGroup;
+  private stock!: StockModel;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
 
   ngOnInit(): void {}
 
   onSubmit() {
-    console.log('Form control value', this.stockForm.value);
+    this.stock = Object.assign({}, this.stockForm.value);
+    console.log('Form control value', this.stock);
+  }
+
+  createForm() {
+    this.stockForm = this.fb.group({
+      name: [null, Validators.required],
+      code: [null, [Validators.required, Validators.minLength(2)]],
+      price: [0, [Validators.required, Validators.min(1)]],
+    });
+  }
+
+  loadStockFromServer() {
+    this.stock = new StockModel('Test ' + counter++, 'TSC', 20, 10);
+    let stockModelForForm: Partial<StockModel> = Object.assign({}, this.stock);
+    delete stockModelForForm.prev_price;
+    delete stockModelForForm.favorite;
+    delete stockModelForForm.exchange;
+    console.log('stockModelForForm', stockModelForForm);
+    this.stockForm.setValue(stockModelForForm);
+  }
+
+  resetStockForm() {
+    this.stockForm.reset();
+  }
+
+  patchStockForm() {
+    this.stock = new StockModel('Test ' + counter++, 'TSC', 20, 10);
+    this.stockForm.patchValue(this.stock);
+  }
+
+  get name() {
+    return this.stockForm.get('name');
+  }
+  get price() {
+    return this.stockForm.get('price');
+  }
+  get code() {
+    return this.stockForm.get('code');
   }
 }
